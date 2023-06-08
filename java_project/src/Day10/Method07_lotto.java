769ee4a91e1ee59fcb99532e43b1e0661230ff06
acejgh <acejgh@gmail.com>
2023-06-08 21:38:10 +0900

    no message

--- java_project/src/Day10/Method07_lotto.java
+++ java_project/src/Day10/Method07_lotto.java
@@ -1,436 +1,178 @@
 package Day10;
 
 public class Method07_lotto {
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		/*
-		 * 로또 번호 생성 (1~45 랜덤으로 생성)
-		 * 당첨 확인
-		 */
-		int lotto[] = new int [7];	//당첨번호+보너스번호 ===> 랜덤 생성
-		int user[] = new int[6];	//사용자번호
-		int bonus = 0;
-	
-		int random45=0;
-		random45 = random();
-		randomArray(lotto);
-//	    bonus = lotto[7]; 
-		randomArray(user);
-		System.out.println();
-		printArray(lotto);
-		System.out.println();
-		printArray(user);
-		System.out.println();
-//		isContain(lotto,user);
-		System.out.println();
-//		isContain(user);
-		
-		System.out.println();
-		int rank = lottoRank(lotto,user);
-		if(rank == -1) {
-			System.out.println("꽝");
-		}else {
-			System.out.println(rank + " 등 당첨");
-		}
-		
-	}	
-	/*
-	 * 1.번호를 랜덤으로 생성하는 기능.
-	 * 1~45까지 램덤생성
-	 * 매개변수: int
-	 * 리턴타입: int 1개
-	 * 매서드명 : ramdom
-	 */
-	public static int random() {
-		//1~45까지의 랜덤수 추출
-		//(int)(Math.random()*n)+start;
-		int random = (int)(Math.random()*45)+1;
-		return random;
-	}
-	
-	
-	
-	
-	/*
-	 * 2. 방금생성한 랜덤을 배열에 저장
-	 * 배열에 1번의 랜덤번호를 저장하는 기능.
-	 * 매개변수 :
-	 * 리턴타입 :
-	 * 메서드명 : randomArray
-	 */
-	public static void randomArray( int[] arr) {
-//		int[] a = null;
-//		a = new int[arr.length];
-		boolean a= isContatin_OMR1(arr);
-		for(int i = 0; i<arr.length; i++) {
-			
-			if ( a == false){
-			arr[i] = random();
-			}
-		}
+   
+   public static void main(String[] args) {
+      /* 로또 번호 생성 (1~45 랜덤으로 생성)
+       * 당첨 확인
+       * */
+      int lotto[] = new int[7]; //당첨번호+보너스번호 => 랜덤생성
+      int user[] = new int[6];  //사용자번호
 
-//		return a; 
-	}
-	
-	public static void randomArray( int[] arr, int a) {
-		int cnt = 0;
-		while(cnt < arr.length) {
-			int r = random();
-			if(!isContain(arr,r)) { //true중복된다.
-				arr[cnt] = r;
-				cnt ++;
-=======
-		/* 로또 번호 생성 (1~45 랜덤으로 생성)
-		 * 당첨확인
-		 * */
-		int lotto[] = new int[7]; //당첨번호+보너스번호 => 랜덤생성
-		int user[] = new int[6]; //사용자번호
-		// lotto 배열과 user 배열을 random값으로 채우기
-		randomArray(lotto);
-		randomArray(user);
-		//print
-		printArray(lotto);
-		printArray(user);
-		
-		//매개변수의 자리에 다른 값이 들어가도 자료형만 맞으면 에러가 나지 않음.
-		int rank = lottoRank(lotto, user);
-		if(rank == -1) {
-			System.out.println("꽝~!!");
-		}else {
-			System.out.println(rank+"등 당첨입니다.~!!");
-		}
-	}
-	/* 1. 번호를 랜덤으로 생성하는 기능.
-	 * 1~45까지 랜덤생성
-	 * 매개변수 : X
-	 * 리턴타입 : 랜덤값 1개 => int
-	 * 메서드명 : random
-	 * */
-	public static int random() {
-		return (int)(Math.random()*10)+1;
-	}
-	
-	/* 2. 배열에 1번의 랜덤번호를 저장하는 기능.
-	 * 매개변수 : 배열 => int arr[]
-	 * 리턴타입 : X => void
-	 * 메서드명 : randomArray
-	 * */
-	public static void randomArray(int arr[]) {
-		// 배열을 받아서 배열에 랜덤 값을 채우기
-		// isContain
-//		for(int i=0; i<arr.length; i++) {
-//			arr[i] = random();
-//		}
-		//user = 5 15 23 0 0 0
-		int cnt = 0; //i를 의미
-		while(cnt < arr.length) {
-			int r = random(); //5
-			if(!isContain(arr, r)) { //true = 중복된다.
-				arr[cnt] = r;
-				cnt++;
->>>>>>> 1f79ec77ed0d00f1aab2af77788ea759628a3dde
-			}
-		}
-	}
-	
-<<<<<<< HEAD
-	
-	/* 3.
-	 * 배열을 콘솔에 출력하는 기능
-	 * 매개변수 :
-	 * 리턴타입 : void
-	 * 메서드명 : printArray
-	 */
-	public static void printArray(int[] arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr) {
-		for(int i = 0; i<arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.length; i++) {
-		System.out.print(arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr[i]+ " ");
-		}
-	}
-	
-	
-	
-	
-	/*
-	 *4. 같은 값이 있는지 확인하는 메서드
-	 *매개변수 :
-	 *리턴타입 :
-	 *메서드명 : isContatin 
-	 */
-	public static void isContatin_JGH(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) { // 오름차순 내림차수는'<'로부등호 바꿈
-					// 교환코드
-					int tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-				}
+      randomArray(lotto);
+      randomArray(user);
+      
+      printArray(lotto);
+      printArray(user);
+      
+      //매개변수의 자리에 다른 값이 들어가도 자료형만 맞으면 에러가 나지 않음.
+      int rank = lottoRank(lotto, user);
+      //랭크 확인
+      if(rank==-1) {
+         System.out.println("꽝");
+      }else {
+         System.out.println(lottoRank(lotto, user)+"등입니다~!");
+      }
+      
+      
+      
+   }
+   
+   /* 1. 번호를 랜덤으로 생성하는 기능.
+    * 1~45까지 랜덤생성
+    * 매개변수 : x
+    * 리턴타입 : x => void
+    * 메서드명 : random
+    * */
+   public static int random() {
+      return (int)(Math.random()*9)+1;
+   }
+   
+   /* 2. 배열에 1번의 랜덤번호를 저장하는 기능.
+    * 매개변수 : 배열 => int arr[]
+    * 리턴타입 : x => void
+    * 메서드명 : randomArray
+    * */
+   
+//   public static void randomArray(int arr[]) {
+//      for(int i = 0; i < arr.length; i++) {
+//         arr[i] = random();
+//      }
+//   }
+   
+   //선생님 풀이
+   public static void randomArray(int arr[]) {
+      int cnt = 0; //i를 의미
+      while(cnt < arr.length) {
+         int r = random();
+         if(!isContain(arr, r)) { //true = 중복된다
+            arr[cnt] = r;
+            cnt++;
+         }
+      }
+   }
+   
+   /* 3. 배열을 콘솔에 출력하는 기능
+    * 매개변수 : 
+    * 리턴타입 :
+    * 메서드명 : printArray
+    * */
+   public static void printArray(int arr[]) {
+      for(int a : arr) {
+         System.out.print(a+" ");
+      }
+      System.out.println();
+   }
+   
+   /* 4. 같은 값이 있는지 확인하는 기능
+    * 매개변수 :
+    * 리턴타입 :
+    * 메서드명 : isContain
+    * */
+   
+//   public static boolean isContain(int arr[]) {
+//      for(int i=0; i<arr.length; i++) {
+//         for(int j=i+1; j<arr.length; j++) {
+//            if(arr[i] == arr[j]) {
+//               return true;
+//            }
+//         }
+//      }
+//      return false;
+//   }
+   
+   //선생님 풀이
+   public static boolean isContain(int arr[], int random) {
+      for(int i=0; i<arr.length; i++) {
+         if(arr[i] == random) {
+            return true;
+         }
+      }
+      return false;
+   }
+   
+   
+   /* 5. 등수를 알려주는 메서드
+    * 매개변수 :
+    * 리턴타입 : int => 등수
+    * 메서드명 : lottoRank
+    * 
+    * -- 당첨기준
+    * 6개일치 : 1등
+    * 5개일치+보너스 : 2등
+    * 5개일치 : 3등
+    * 4개일치 : 4등
+    * 3개일치 : 5등
+    * 나머지는 꽝
+    * */
+   public static int lottoRank(int lotto[], int user[]) {
+      //lotto=7개, user=6개
+      //lotto와 user는 개수가 다르기 때문에 바뀌어서 들어오면 error
+      if(lotto.length <= user.length) {
+         return -1;
+      }
+      
+      int cnt = 0; //당첨번호와 일치하는 번호 개수
+      int rank;
+      
+      //보너스번호를 제외한 번호 일치 개수 확인
+//      for(int i=0; i<lotto.length-1; i++) {
+//         for(int j=0; j<user.length; j++) {
+//            if(lotto[i]==user[j]) {
+//               cnt++;
+//            }
+//         }
+//      }//for end
+      //선생님 풀이
+      for(int i=0; i<lotto.length; i++) {
+         if(isContain(user, lotto[i])) {
+            cnt++;
+         }
+      }//for end
+      
+      //번호 일치 개수에 따른 등수 (보너스 제외)
+//      switch(cnt){
+//      case 6: rank = 1; break;
+//      case 5: rank = 3; break;
+//      case 4: rank = 4; break;
+//      case 3: rank = 5; break;
+//      default:
+//         rank = 0;
+//      }//switch end
+//      
+//      //번호 5개가 일치할 경우, 보너스 번호 일치 여부 확인
+//      if(cnt==5) {
+//         for(int i=0; i<user.length; i++) {
+//            if(lotto[6]==user[i]) {
+//               rank = 2;
+//            }
+//         }
+//      }//if end
+      //선생님 풀이
+      switch(cnt){
+      case 6: return 1;
+      case 5:
+         if(isContain(user, lotto[lotto.length-1])){
+            return 2;
+         }else {
+            return 3;
+         }
+      case 4: return 4;
+      case 3: return 5;
+      default:
+         return -1;
+      }//switch end
+      
+   }//lottoRank end
 
-				if (arr[i] == arr[i + 1]) {
-					arr[i + 1] = random();
-					isContatin_JGH(arr);
-					System.out.println("같은값 확인되어서 다른 값으로 교체 후 재귀?호출 겹치는거 안나올때까지 돌것임");
-				}
-			}
-		} // end for
-
-	}
-	
-	public static boolean isContatin_OMR1(int[] arr) {
-		boolean a = true;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) { // 오름차순 내림차수는'<'로부등호 바꿈
-					// 교환코드
-					int tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-				}
-
-				if (arr[i] == arr[i + 1]) {
-					arr[i + 1] = random();
-					isContatin_JGH(arr);
-					System.out.println("같은값 확인되어서 다른 값으로 교체 후 재귀?호출 겹치는거 안나올때까지 돌것임");
-					a = true;
-				}else {
-					a = false;
-				}
-			}
-		} // end for
-		return a;
-	}
-	
-	public static boolean isContain(int[] arr, int r) {
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i]== random()) {
-				return true;
-			} //for문과 상관없이 메서드 종료
-=======
-	/* 3. 배열을 콘솔에 출력하는 기능
-	 * 매개변수 : 배열 => int arr[]
-	 * 리턴타입 : x => void
-	 * 메서드명 : printArray
-	 * */
-	public static void printArray(int arr[]) {
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();
-	}
-	
-	/* 4. 같은 값이 있는지 확인하는 기능
-	 * 매개변수 : 배열, 랜덤값
-	 * 리턴타입 : boolean
-	 * 메서드명 : isContain
-	 * */
-	public static boolean isContain(int arr[], int random) {
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i] == random) {
-				return true; // for문과 상관없이 메서드 종료
-			}
->>>>>>> 1f79ec77ed0d00f1aab2af77788ea759628a3dde
-		}
-		return false;
-	}
-	
-	
-<<<<<<< HEAD
-	
-	
-	/*
-	 *4. 같은 값이 있는지 확인하는 메서드
-	 *매개변수 : 배열, 랜덤값
-	 *리턴타입 : boolean
-	 *메서드명 : isContatin_OMR 
-	 */
-	public static void isContatin_OMR(int[] arr) {
-		
-		
-	}
-	
-	
-	
-	
-	/*
-	 * 5. 등수를 알려주는 메서드
-	 * 매개변수 : lotto[], user[]
-	 * 리턴타입 :
-	 * 메서드명 : lottoRank
-	 * -- 당첨기준
-	 * 6개 일치 : 1등
-	 * 5개 일치 + 보너스 :  2등
-	 * 5개 일치 : 3등
-	 * 4개 일치 : 4등
-	 * 3개 일치 : 5등
-	 * 나머지는 꽝
-	 */
-	public static void lottoRank_JGH(int[] arr0 , int[] arr1) {
-		for(int i=0; i<arr0.length; i++) {
-			for(int j=i+1; j<arr0.length; j++) {
-				if(arr0[i]>arr0[j]) {  //오름차순        내림차수는'<'로부등호 바꿈
-					//교환코드
-					int tmp = arr0[i];
-					arr0[i] = arr0[j];
-					arr0[j] = tmp;
-				}
-			}
-		}//end for
-		for(int i=0; i<arr1.length; i++) {
-			for(int j=i+1; j<arr1.length; j++) {
-				if(arr1[i]>arr1[j]) {  //오름차순        내림차수는'<'로부등호 바꿈
-					//교환코드
-					int tmp = arr1[i];
-					arr1[i] = arr1[j];
-					arr1[j] = tmp;
-				}
-			}
-		}//end for
-	
-		int count = 0;
-		
-		for(int i=0 ; i<6 ; i++ ) {
-			
-			for(int j=0; j<6; j++) {
-			
-				if(arr0[i]==arr1[j]) {
-					count++;	
-				}
-				
-			}
-		}
-		int bonus_count = 0;
-		
-			
-			for(int j=0; j<6; j++) {
-			
-				if(arr0[j]==bonus_count) {
-					bonus_count++;	
-				}
-				
-			}
-		
-		
-		System.out.println(count + " 개 일치 ");
-
-		if (count == 6) {
-			System.out.println(" 6개 일치 : 1등");
-		} else if (count == 5 && bonus_count == 1) {
-			System.out.println("5개 보너스1개 일치 2등");
-		} else if (count == 5) {
-			System.out.println(" 5개 일치 : 3등");
-		} else if (count == 4) {
-			System.out.println(" 4개 일치 : 4등");
-		} else if (count == 3) {
-			System.out.println(" 3개 일치 : 5등");
-		} else if (count == 2) {
-			System.out.println(" 2개 일치 : 등수없음 꽝");
-		} else if (count == 1) {
-			System.out.println(" 1개 일치 : 등수없음 꽝");
-		} else {
-			System.out.println("0개일치 그냥 다 꽝");
-		}
-	
-	}
-	
-	
-	
-	/*
-	 * 5. 등수를 알려주는 메서드
-	 * 매개변수 : lotto[], user[]
-	 * 리턴타입 :
-	 * 메서드명 : lottoRank
-	 * -- 당첨기준
-	 * 6개 일치 : 1등
-	 * 5개 일치 + 보너스 :  2등
-	 * 5개 일치 : 3등
-	 * 4개 일치 : 4등
-	 * 3개 일치 : 5등
-	 * 나머지는 꽝
-	 */
-	public static int lottoRank(int[] lotto , int[] user) {
-		//lotto = 7개
-		//user = 6개
-		//lotto와 user는 개수가 다르기 떄문에 바뀌어서 들어오면 error
-		if(lotto.length <= user.length) {
-			return -1;
-		}
-		int cnt = 0;
-		for(int i = 0; i < user.length; i++) {
-			if(isContain(user, lotto[i])) {
-				cnt++;
-			}
-		}
-		
-		//보너스 제외 숫자
-		switch(cnt) {
-		case 6: return 1;
-		case 5: 
-=======
-	/* 5. 등수를 알려주는 메서드 
-	 * 매개변수 : lotto[], user[]
-	 * 리턴타입 : int => 등수를 리턴
-	 * 메서드명 : lottoRank
-	 * -- 당첨기준
-	 * 6개일치 : 1등
-	 * 5개일치+보너스 :2등
-	 * 5개일치 : 3등
-	 * 4개일치 : 4등
-	 * 3개일치 : 5등
-	 * 나머지는 꽝
-	 * */
-	public static int lottoRank(int lotto[], int user[]) {
-		//lotto =7개, user = 6개
-		//lotto와 user는 개수가 다르기 때문에 바뀌어서 들어오면 error
-		if(lotto.length <= user.length) {
-			return -1;
-		}
-		int cnt =0; //일치 개수(보너스 제외)
-		for(int i=0; i<user.length; i++) { //보너스를 제외시키기 위해서
-			if(isContain(user, lotto[i])) {
-				cnt++;
-			}	
-		}
-		// cnt : 보너스 제외 맞춘개수
-		switch(cnt) {
-		case 6: return 1;
-		case 5:
->>>>>>> 1f79ec77ed0d00f1aab2af77788ea759628a3dde
-			if(isContain(user, lotto[lotto.length-1])) {
-				return 2;
-			}else {
-				return 3;
-			}
-<<<<<<< HEAD
-		
-		case 4 : return 4;
-		case 3 : return 5;
-		default:
-			return -1;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	}
-	
-	
-	
-	
-	
-	
-=======
-		case 4: return 4;
-		case 3: return 5;
-		default:
-			return -1;
-		}
-	}
-	
->>>>>>> 1f79ec77ed0d00f1aab2af77788ea759628a3dde
 }
